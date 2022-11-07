255e77951efd5b48871a4d6fb76a298489ff64b1
Mateo Vasquez <mateovz536@gmail.com>
2022-11-07 15:10:59 +0100

    añade scrolltoview al navegar con botones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const DESKTOP = 1024;
-
-    const menuButtons = document.querySelectorAll('nav a.hidden');
+    const menuButtonsMobile = document.querySelectorAll('#nav-mobile a.hidden');
+    const menuButtonsDesktop = document.querySelectorAll('#nav-desktop a');
+    const titleButton = document.querySelector('h1');
     const openNavButton = document.querySelector('#show-nav');
     
     // carga
     window.addEventListener('load', () => {
         hiddenNav();
+        onScrollTo(window.location.hash);
     });
     // redimensionar
     window.addEventListener('resize', () => {
@@ -14,22 +15,33 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     
     // botones version movil
-    menuButtons.forEach(button => {
-        const nav = document.querySelector('nav');
-        button.addEventListener('click', () => {
+    menuButtonsMobile.forEach(button => {
+        const nav = document.querySelector('#nav-mobile');
+        button.addEventListener('click', (e) => {
             hiddenElement(nav);
         })
+    });
+
+    // botones version escritorio
+    menuButtonsDesktop.forEach(button => {
+        button.addEventListener('click', (e) => {
+            scrollToElement(e);
+        })
+    });
+
+    titleButton.addEventListener('click', (e) =>{
+        scrollToElement(e);
     });
     
     // boton open menu header
     openNavButton.addEventListener('click', () => {
-        const nav = document.querySelector('nav');
+        const nav = document.querySelector('#nav-mobile');
         visibleElement(nav);
     });
     
     // ocultar nav
     function hiddenNav(){
-        const nav = document.querySelector('nav');
+        const nav = document.querySelector('#nav-mobile');
         const hash = window.location.hash;
     
         if(hash !== '' && hash !== '#menu'){
@@ -45,5 +57,26 @@ document.addEventListener('DOMContentLoaded', () => {
     // retira clase hidden para mostrar elemento
     function visibleElement(element){
         element.classList.remove('hidden');
+    }
+
+    function scrollToElement(e){
+        e.preventDefault();
+        const hash = e.target.getAttribute("href");
+        onScrollTo(hash);
+    }
+
+    function onScrollTo(hash){
+        setTimeout(() => {
+            const element = document.querySelector(hash);
+    
+            element.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }, 100);
+
+        setTimeout(() => {
+            window.location.hash = hash;
+        }, 600);
     }
 });
